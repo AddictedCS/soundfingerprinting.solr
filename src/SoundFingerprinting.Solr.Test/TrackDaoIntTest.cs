abe55cf3146c1257ef5d844ba6440eb6b6a8127a
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2016-09-30 15:26:56 +0300

    Adding some tests

---
+++ src/SoundFingerprinting.Solr.Test/TrackDaoIntTest.cs
@@ -0,0 +1,19 @@
+﻿namespace SoundFingerprinting.Solr.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using SoundFingerprinting.DAO.Data;
+
+    [TestClass]
+    public class TrackDaoIntTest
+    {
+        [TestMethod]
+        public void ShouldInsertTrack()
+        {
+            var track = new TrackData("isrc", "artist", "title", "album", 1986, 3.6);
+            var trackDao = new TrackDao();
+            var reference = trackDao.InsertTrack(track);
+            Assert.IsNotNull(reference);
+        }
+    }
+}
