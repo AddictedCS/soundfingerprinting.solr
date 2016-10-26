07b82318f8b03d8eb0df067d636551882eba2e7b
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2016-10-26 15:59:39 +0300

    Implementing all methods from Track data access object.

--- src/SoundFingerprinting.Solr/TrackDao.cs
+++ src/SoundFingerprinting.Solr/TrackDao.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     using SolrNet;
 
@@ -12,7 +13,7 @@
 
     internal class TrackDao : ITrackDao
     {
-        private ISolrOperations<TrackDTO> solr;
+        private readonly ISolrOperations<TrackDTO> solr;
 
         public TrackDao() : this(DependencyResolver.Current.Get<ISolrOperations<TrackDTO>>())
         {
@@ -26,7 +27,7 @@
         public IModelReference InsertTrack(TrackData track)
         {
             Guid id = Guid.NewGuid();
-            var dto = new TrackDTO()
+            var dto = new TrackDTO
                 {
                     Id = id.ToString(),
                     Album = track.Album,
@@ -45,27 +46,75 @@
 
         public TrackData ReadTrack(IModelReference trackReference)
         {
-            throw new System.NotImplementedException();
+            var trackId = SolrModelReference.GetId(trackReference);
+            var query = new SolrQuery(string.Format("trackId:{0}", trackId));
+
+            var results = solr.Query(query);
+            return FirstFromResultSet(results);
         }
 
         public int DeleteTrack(IModelReference trackReference)
         {
-            throw new System.NotImplementedException();
+            solr.Delete(SolrModelReference.GetId(trackReference));
+            solr.Commit();
+            return 1;
         }
 
         public IList<TrackData> ReadTrackByArtistAndTitleName(string artist, string title)
         {
-            throw new System.NotImplementedException();
+            var query = new SolrQuery(string.Format("title:{0} AND artist:{1}", title, artist));
+            var results = solr.Query(query);
+            return AllFromResultSet(results);
         }
-
+        
         public TrackData ReadTrackByISRC(string isrc)
         {
-            throw new System.NotImplementedException();
+            var query = new SolrQuery(string.Format("isrc:{0}", isrc));
+            var results = solr.Query(query);
+
+            return FirstFromResultSet(results);
         }
 
         public IList<TrackData> ReadAll()
         {
-            throw new System.NotImplementedException();
+            var query = new SolrQuery("*:*");
+            var results = solr.Query(query);
+            return AllFromResultSet(results);
+        }
+
+        private static TrackData Convert(TrackDTO dto)
+        {
+            var track = new TrackData(
+                dto.ISRC,
+                dto.Artist,
+                dto.Title,
+                dto.Album,
+                dto.ReleaseYear,
+                dto.TrackLengthSec,
+                new SolrModelReference(dto.Id));
+            track.GroupId = dto.GroupId;
+            return track;
+        }
+
+        private static TrackData FirstFromResultSet(ICollection<TrackDTO> results)
+        {
+            if (results.Count > 0)
+            {
+                var dto = results.First();
+                return Convert(dto);
+            }
+
+            return default(TrackData);
+        }
+
+        private static IList<TrackData> AllFromResultSet(ICollection<TrackDTO> results)
+        {
+            if (results.Count == 0)
+            {
+                return new List<TrackData>();
+            }
+
+            return results.Select(Convert).ToList();
         }
     }
 }
