dcd5e886999f53abfe83fdb56f112ea9036dcef6
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2016-10-27 12:17:45 +0300

    Adding solr query builder. This will imply Range function to properly select the item from Solr. Details here: http://lucene.apache.org/solr/6_2_1/solr-core/org/apache/solr/search/FunctionRangeQParserPlugin.html

---
+++ src/SoundFingerprinting.Solr/SolrQueryBuilder.cs
@@ -0,0 +1,16 @@
+﻿namespace SoundFingerprinting.Solr
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class SolrQueryBuilder
+    {
+        public string BuildReadQueryFor(long[] hashBins, int thresholdVotes)
+        {
+            List<string> termFreqs = hashBins.Select((hash, index) => string.Format("termfreq(hashTable_{0},'{1}')", index, hash)).ToList();
+            string joined = string.Join(",", termFreqs);
+            string queryString = string.Format("{{!frange l={0} u={1}}}sum({2})", thresholdVotes, hashBins.Length, joined);
+            return queryString;
+        }
+    }
+}
