3568f0091761a4840a82a7392843d329dcb541f4
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2016-10-27 12:15:55 +0300

    Adding converter to / from Solr dictionary. Hashes as stored in Solr as Dictionary (thoroughly mapped by SolrNet library).

---
+++ src/SoundFingerprinting.Solr.Test/DictionaryToHashConverterTest.cs
@@ -0,0 +1,31 @@
+﻿namespace SoundFingerprinting.Solr.Tests
+{
+    using System.Collections.Generic;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DictionaryToHashConverterTest
+    {
+        private readonly DictionaryToHashConverter dictionaryToHashConverter = new DictionaryToHashConverter();
+
+        [TestMethod]
+        public void ShouldConvertToAndFrom()
+        {
+            long[] hashBins = new long[] { 3, 4, 5, 7, 8 };
+            Dictionary<int, long> hashTables = dictionaryToHashConverter.FromHashes(hashBins);
+
+            Assert.AreEqual(3, hashTables[0]);
+            Assert.AreEqual(4, hashTables[1]);
+            Assert.AreEqual(5, hashTables[2]);
+            Assert.AreEqual(7, hashTables[3]);
+            Assert.AreEqual(8, hashTables[4]);
+
+            long[] actual = dictionaryToHashConverter.FromSolrDictionary(hashTables);
+            for (int i = 0; i < hashBins.Length; ++i)
+            {
+                Assert.AreEqual(hashBins[i], actual[i]);
+            }
+        }
+    }
+}
