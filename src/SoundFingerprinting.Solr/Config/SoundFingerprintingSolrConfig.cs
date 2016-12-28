3f429036e070f384e8ac8d354feb9eef89cc86be
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2016-12-28 22:53:19 +0200

    Adding "SoundFingerprintingConfigurationSection" which will allow specifying "preferLocalShards" and query batch size flags.

---
+++ src/SoundFingerprinting.Solr/Config/SoundFingerprintingSolrConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace SoundFingerprinting.Solr.Config
+{
+    internal class SoundFingerprintingSolrConfig : ISoundFingerprintingSolrConfig
+    {
+        public SoundFingerprintingSolrConfig(int queryBatchSize, bool preferLocalShards)
+        {
+            QueryBatchSize = queryBatchSize;
+            PreferLocalShards = preferLocalShards;
+        }
+
+        public int QueryBatchSize { get; private set; }
+
+        public bool PreferLocalShards { get; private set; }
+    }
+}
