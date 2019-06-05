6f22d2ec5bfcb93608eb41a9fd417d969b525871
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2019-06-05 10:20:13 +0300

    Fixing paths for dotnet 3.x

--- build.cmd
+++ build.cmd
@@ -6,4 +6,4 @@ if "%config%" == "" (
 
 dotnet restore .\src\SoundFingerprinting.Solr.sln
 dotnet test .\src\SoundFingerprinting.Solr.Tests\SoundFingerprinting.Solr.Tests.csproj -c %config%
-dotnet pack .\src\SoundFingerprinting.Solr\SoundFingerprinting.Solr.csproj -c %config% -o ..\..\build
+dotnet pack .\src\SoundFingerprinting.Solr\SoundFingerprinting.Solr.csproj -c %config% -o build
